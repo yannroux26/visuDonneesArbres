24b7138d11dfae3b875eabdb2769d9a8892d6a71
Yann ROUX <116753299+yannroux26@users.noreply.github.com>
2025-01-10 10:37:40 +0100

    fin 4 étapes decomp/recomp

--- perteDeTemps.py
+++ perteDeTemps.py
@@ -1,3 +1,7 @@
+import random
+from math import sqrt
+import matplotlib.pyplot as plt
+
 def Decomp(tab):
     l = len(tab)
     res = tab.copy()
@@ -5,10 +9,9 @@ def Decomp(tab):
         for i in range(0, l, 2):
             res[i//2] = (tab[i] + tab[i + 1])/2
             res[l -1 -i//2] = (tab[i] - tab[i + 1])/2
-        print(res)
+        #print(res)
         l = l // 2
         tab = res.copy()
-    print("fin decomposition")
     return res
 
 def Recomp(tab):
@@ -19,12 +22,61 @@ def Recomp(tab):
         for i in range(0, l//2, 1):
             res[i*2] = tab[i] + tab[l -1 -i]
             res[i*2 + 1] = tab[i] - tab[l -1 -i]
-        print(res)
+        #print(res)
         l = l * 2
         tab = res.copy()
-    print("fin recomposition")
     return res
 
+def RecompPart(tab,epsilon) :
+    for i in range(1,len(tab),1):
+        if (abs(tab[i])<epsilon):
+            tab[i]=0
+    return tab
+
+def erreur(tab1,tab2):
+    e=0
+    for i in range (len(tab1)):
+        e += (tab1[i] - tab2[i]) ** 2
+    return sqrt(e)
+
 if __name__ == "__main__":
-    tab = [2, 7, 9, 3,1,8,2,6]
-    Recomp(Decomp(tab))
+    epsilon = 10
+    maxVal = 100
+    tab = [random.randint(0, maxVal) for _ in range(32)]
+    
+    # Décomposition et recomposition
+    decompTab = Decomp(tab.copy())
+    recompPartTab = RecompPart(decompTab.copy(),epsilon)
+    res = Recomp(recompPartTab.copy())
+    print("tab initial: ",tab)
+    #print("tab décomposé: ",decompTab)
+    #print("tab avec seuil: ",recompPartTab)
+    print("tab recomposé: ",res)
+    
+    e = erreur(tab,res)
+    print("erreur : ",e)
+    
+    # histogramme des valeurs absolues de tab
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.hist([abs(val) for val in decompTab], bins=100)
+    plt.title('Histogramme des valeurs absolues de tab')
+    
+    # graphique des erreurs en fonction de epsilon
+    x = []
+    y = []
+    for i in range(0, maxVal, 1):
+        epsilon = i
+        recompPartTab = RecompPart(decompTab.copy(),epsilon)
+        res = Recomp(recompPartTab.copy())
+        e = erreur(tab,res)
+        x.append(epsilon)
+        y.append(e)
+        
+    plt.subplot(1, 2, 2)
+    plt.plot(x, y)
+    plt.title('Graphique des erreurs en fonction de epsilon')
+    plt.tight_layout()
+    plt.show()
+    
+    
